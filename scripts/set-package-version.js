6707da9901b5b58dd14dbe4c2b7bb5f5f4ab6b67
Nate Totten <nate@zuplo.com>
2022-02-21 16:11:02 -0500

    script args

--- scripts/set-package-version.js
+++ scripts/set-package-version.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const fs = require('fs')
 
-const pkgPath = path.resolve(process.cwd(), process.argv[0])
+const pkgPath = path.resolve(process.cwd(), process.argv[2])
 const json = fs.readFileSync(pkgPath, 'utf-8')
 const pkg = JSON.parse(json)
 pkg.version = process.env.GITHUB_REF.replace('refs/tags/v', '')
